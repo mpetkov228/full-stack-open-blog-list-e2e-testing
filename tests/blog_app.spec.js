18fade2539cc6dab01f24c9a4d84dc473bf524e6
MihailPetkov96 <mpetkov228@gmail.com>
2024-09-26 18:09:41 +0300

    Test successfull login

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -8,4 +8,14 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('log in to application')).toBeVisible();
   });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('root');
+      await page.getByTestId('password').fill('secret');
+      await page.getByRole('button', { name: 'Log in' }).click();
+
+      await expect(page.getByText('Superuser logged in')).toBeVisible();
+    });
+  });
 });
